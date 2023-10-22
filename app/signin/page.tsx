c763cee987faac0197d91d0830cd9e43db167cda
Salva <s@salva.io>
2023-10-22 03:44:48 -0300

    wip: adding signin page.

---
+++ app/signin/page.tsx
@@ -0,0 +1,13 @@
+import { Layout } from "../components/Layout";
+
+type Props = {};
+
+const SignIn = ({}: Props): JSX.Element => {
+  return (
+    <Layout>
+      <h2>Sign In</h2>
+    </Layout>
+  );
+};
+
+export default SignIn;
