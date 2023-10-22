3ad0c9a97cd89bbc3c3d569032cd5c313096cd1c
Salva <s@salva.io>
2023-10-22 05:44:56 -0300

    chore: add sign in buttons.

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,7 +1,8 @@
 import Image from "next/image";
-
+import css from 'classnames';
 import { Layout } from "../components/Layout";
 import { Heading } from "../components/Heading";
+import { Button } from "../components/Button";
 
 type Props = {};
 
@@ -19,7 +20,16 @@ const SignIn = ({}: Props): JSX.Element => {
         />
       </div>
 
-      <Heading variant="v100" style={{ width: '320px' }}>The only productivity app you need</Heading>
+      <Heading variant="v100" style={{ width: '320px', marginBottom: '120px' }}>The only productivity app you need</Heading>
+
+      <Button title="Sign in with Email" theme="primary" style={{ margin: '18px 0' }} />
+
+      <div style={{ display: 'flex', gap: '18px' }}>
+        <Button style={{ flex: 1 }} title="Google" />
+        <Button style={{ flex: 1 }} title="Apple ID" />
+      </div>
+
+      <p style={{ textAlign: 'center', marginTop: '8px', color: '#a8a8a8' }}>By Continuing you agree to the Terms and Condition</p>
     </Layout>
   );
 };
