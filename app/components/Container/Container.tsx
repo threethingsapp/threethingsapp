eda176a6d4ceba1d989d23688f3c1eea3f62fdc3
Salva <s@salva.io>
2023-11-04 18:30:03 -0300

    styles: update container styles.

--- app/components/Container/Container.tsx
+++ app/components/Container/Container.tsx
@@ -2,13 +2,15 @@ import css from 'classnames';
 import styles from './Container.module.css';
 
 type Props = {
-  children: JSX.Element;
-  height?: string;
+  children: JSX.Element | JSX.Element[];
+  height?: number;
+  className?: string;
 };
 
-const Container = ({ children, height }: Props): JSX.Element => {
+const Container = ({ children, height, className }: Props): JSX.Element => {
+
   return (
-    <div className={css(styles.wrapper)} style={{ height }}>
+    <div className={css(styles.wrapper, className)} style={{ minHeight: `${height}px` ?? '100%' }}>
       {children}
     </div>
   );
