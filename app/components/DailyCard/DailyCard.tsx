a72675d0b7de753d0fb8f68fdc5923ea56915f2d
Salva <s@salva.io>
2023-11-04 18:33:20 -0300

    wip: updating daily card.

--- app/components/DailyCard/DailyCard.tsx
+++ app/components/DailyCard/DailyCard.tsx
@@ -1,11 +1,21 @@
-import { Container } from "../Container";
+import { Container, Heading, ProgressBar } from "..";
+import { RandomPeople } from "../RandomPeople/RandomPeople";
+import styles from "./DailyCard.module.css";
 
 type Props = {};
 
 const DailyCard = ({}: Props): JSX.Element => {
   return (
-    <Container height="120px">
-      <h2>daily card</h2>
+    <Container height={120} className={styles.wrapper}>
+      <div>
+        <Heading variant="v500">Daily progress</Heading>
+      </div>
+
+      <small style={{ color: "#c1c1c1ff", marginBottom: "8px" }}>
+        Here you can see your daily task progress
+      </small>
+
+      <ProgressBar progress={76} />
     </Container>
   );
 };
