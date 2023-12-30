3d94ddb5217a28da8fef155da6b722518b00e6d6
Salva <s@salva.io>
2023-12-30 20:26:10 -0300

    chore: add switch action.

--- app/components/Switch/Switch.tsx
+++ app/components/Switch/Switch.tsx
@@ -1,13 +1,24 @@
-import styles from './Switch.module.scss';
+"use client";
+
+import { useState } from "react";
+import css from "classnames";
+import styles from "./Switch.module.scss";
 
 type Props = {};
 
 const Switch = ({}: Props): JSX.Element => {
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+
+  const handleToggle = () => setIsSelected(!isSelected);
+
   return (
-    <div className={styles.wrapper}>
+    <div
+      onClick={handleToggle}
+      className={css(styles.wrapper, { [styles.selected]: isSelected })}
+    >
       <div className={styles.knob}></div>
     </div>
-  )
+  );
 };
 
 export { Switch };
